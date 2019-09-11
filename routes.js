b8b08e169f82de4e3000c4b22788facda41d09e0
BarbieDoLLxo <barbie.slumerican@gmail.com>
2019-09-10 21:09:22 -0600

    ahhhh

--- routes.js
+++ routes.js
@@ -1,10 +1,30 @@
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "home.html"));
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitListData");
+
+module.export = function(app){
+
+    app.get("api/tables", function(req,res){
+        res.json(tableData);
+    });
+     app.get("api/waitList", function(req,res){
+        res.json(waitListData);
+        });
+
+        app.post("api/tables", function(req,res){
+            if (tableData.length < 5) {
+                tableData.push(req.body)
+                res.json(true);
+
+            }
+           else{
+               waitListData.push(req.body);
+               res.json(false);
+           }
+        });
+app.post("api/clear", function() {
+tableData=[];
+waitListData=[];
+console.log(tableData);
 });
-app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, "reserve.html"));
-});
-app.get("/table", function(req, res){
-    res.sendFile(path.join(__dirname, "table.html"));
-});
+};
 
